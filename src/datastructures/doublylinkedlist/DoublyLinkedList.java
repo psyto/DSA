14d42909bcaa21e1edc49137bdf8ee701e51b075
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-02-21 14:05:35 +0900

    impemented DoublyLinkedList

--- src/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ src/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -42,4 +42,51 @@ public class DoublyLinkedList {
         }
         length++;
     }
+
+    public Node removeLast() {
+        if (length == 0) {
+            return null;
+        }
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = temp.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) {
+            return null;
+        }
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            temp.next = null;
+            head.prev = null;
+        }
+        length--;
+        return temp;
+    }
 }
--- src/datastructures/doublylinkedlist/Main.java
+++ src/datastructures/doublylinkedlist/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(1);
         myDLL.append(2);
+        myDLL.append(3);
+        myDLL.removeLast();
+        myDLL.prepend(10);
+        myDLL.removeFirst();
         myDLL.printList();
     }
 
