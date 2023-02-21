40a97573fd65d912740c32cd6777e24d79dcf849
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-02-21 19:15:06 +0900

    start implementing Stack

---
+++ src/datastructures/stack/Main.java
@@ -0,0 +1,8 @@
+package datastructures.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack myStack = new Stack(1);
+        myStack.printStack();
+    }
+}
---
+++ src/datastructures/stack/Stack.java
@@ -0,0 +1,29 @@
+package datastructures.stack;
+
+public class Stack {
+    Node top;
+    int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+}
