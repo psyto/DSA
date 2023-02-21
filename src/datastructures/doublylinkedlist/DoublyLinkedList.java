36add6c1d612657a340144b1773a67bcf7dccba7
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-02-21 11:42:37 +0900

    implemented constructor for DoublyLinkedList

---
+++ src/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -0,0 +1,45 @@
+package datastructures.doublylinkedlist;
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+}
---
+++ src/datastructures/doublylinkedlist/Main.java
@@ -0,0 +1,10 @@
+package datastructures.doublylinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList myDoublyLinkedList = new DoublyLinkedList(1);
+        myDoublyLinkedList.append(2);
+        myDoublyLinkedList.printList();
+    }
+
+}
