07880231b95dce9f58ce27646c88f03946779d0f
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-03-02 18:45:52 +0900

    implemented removeVertex for graph

--- src/datastructures/graph/Main.java
+++ src/datastructures/graph/Main.java
@@ -8,7 +8,9 @@ public class Main {
         myGraph.addVertex("C");
         myGraph.addEdge("A", "B");
         myGraph.addEdge("B", "C");
+        myGraph.addEdge("A", "C");
         myGraph.removeEdge("A", "B");
+        myGraph.removeVertex("A");
         myGraph.printGraph();
     }
 }
