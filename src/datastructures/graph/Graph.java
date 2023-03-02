923c2046a7835cafc09db3086d4df01651047941
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2023-03-02 18:22:24 +0900

    start implementing graph

---
+++ src/datastructures/graph/Graph.java
@@ -0,0 +1,20 @@
+package datastructures.graph;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Graph {
+    HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+    public boolean addVertex(String vertex) {
+        if (adjList.get(vertex) == null) {
+            adjList.put(vertex, new ArrayList<String>());
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/datastructures/graph/Main.java
@@ -0,0 +1,9 @@
+package datastructures.graph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph myGraph = new Graph();
+        myGraph.addVertex("A");
+        myGraph.printGraph();
+    }
+}
